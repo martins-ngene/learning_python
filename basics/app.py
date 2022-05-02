f38ac11475fe6484a1658c5b094cd03aecae4a69
Martins Ngene <martinsngene@gmail.com>
2022-05-02 16:34:53 +0100

    basics of python

---
+++ basics/app.py
@@ -0,0 +1,12 @@
+# Hello World In Python
+
+print("Hello World")
+
+
+# Drawing A Shape
+# ----
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/---|")
+
---
+++ basics/variablesAndDatatypes.py
@@ -0,0 +1,6 @@
+
+
+print("There once was a man named John,")
+print("he was 35 years old.")
+print("He really liked the name John,")
+print("but didn't like being 35.")
