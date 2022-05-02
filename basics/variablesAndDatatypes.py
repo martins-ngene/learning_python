b842816701b0b6d1dd120c20bdae2eeac1edfb30
Martins Ngene <martinsngene@gmail.com>
2022-05-02 16:53:31 +0100

    variables and data types in python

--- basics/variablesAndDatatypes.py
+++ basics/variablesAndDatatypes.py
@@ -1,6 +1,15 @@
+# Variables and Data types
+# ____
 
+character_name = "Tom"
+character_age = 50
+character_height = 6.2
+is_male = True
 
-print("There once was a man named John,")
-print("he was 35 years old.")
-print("He really liked the name John,")
-print("but didn't like being 35.")
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + character_age + ".")
