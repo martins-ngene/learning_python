db5a5aca6ae43a9082f5f4c1cb55e9b20bf92698
Martins Ngene <martinsngene@gmail.com>
2022-05-03 15:03:04 +0100

    Working With Numbers

---
+++ basics/workingWithNumbers.py
@@ -0,0 +1,15 @@
+# Working with numbers, methods and numerical operations
+
+# Number To String
+num = 5
+
+print(str(num) + "2")
+
+# Getting the absolute value of numbers
+
+negative_number = -5
+print(abs(negative_number))
+
+# Raising numbers to power
+
+print(pow(5, 2))
