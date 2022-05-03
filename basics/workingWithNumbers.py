9afd165183a0415308315866f8909ee03801f8e1
Martins Ngene <martinsngene@gmail.com>
2022-05-03 15:57:53 +0100

    Working With Numbers

--- basics/workingWithNumbers.py
+++ basics/workingWithNumbers.py
@@ -2,14 +2,21 @@
 
 # Number To String
 num = 5
-
 print(str(num) + "2")
 
 # Getting the absolute value of numbers
-
 negative_number = -5
 print(abs(negative_number))
 
 # Raising numbers to power
-
 print(pow(5, 2))
+
+# Return Larger Number
+print(max(10, 24))
+
+# Return Smaller Number
+print(min(10, 24))
+
+# Rounds up or rounds off numbers as the case may be
+print(round(10.2))
+
