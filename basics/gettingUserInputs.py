846ae8f9775ac16f9f229b17e087a6b8dadc54d8
Martins Ngene <martinsngene@gmail.com>
2022-05-05 11:31:13 +0100

    basics of getting user inputs completed

---
+++ basics/gettingUserInputs.py
@@ -0,0 +1,8 @@
+# Getting user inputs and storing them to a variable
+name = input("Enter your name: ")
+print("Hello " + name + "!")
+
+# Getting user inputs and storing them to a variable
+email = input("Enter your Email: ")
+password = input("Enter your Password: ")
+print("Hello, your login to your account, " + email + " is successful!")
