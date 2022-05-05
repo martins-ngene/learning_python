4fd3c619845616478fc68fc78920c9654cb79438
Martins Ngene <martinsngene@gmail.com>
2022-05-05 12:01:54 +0100

    [update]: Built a Mad Libs Game

---
+++ projects/madLibsGame.py
@@ -0,0 +1,9 @@
+#  Building A Mad Libs Game
+
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(pluralNoun + " are Blue")
+print("I love " + celebrity)
