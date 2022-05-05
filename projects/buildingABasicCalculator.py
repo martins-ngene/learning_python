25040ec99200ed3e51c447f3f40caf2db135e1ec
Martins Ngene <martinsngene@gmail.com>
2022-05-05 11:44:03 +0100

    basic calculator completed

---
+++ projects/buildingABasicCalculator.py
@@ -0,0 +1,13 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+# Use the int() function to convert the value of a variable to a whole number
+# wholeNumber = int(num1)
+# print(wholeNumber)
+
+#  Use the float() function to convert the value of a variable to a decimal number
+# decimalNumber = float(num2)
+# print(decimalNumber)
+
+result = float(num1) + float(num2)
+print(result)
