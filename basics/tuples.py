558b561a457515e29ffb313185b234c24f7607e5
Martins Ngene <martinsngene@gmail.com>
2022-05-06 14:36:06 +0100

    basics of tuples

---
+++ basics/tuples.py
@@ -0,0 +1,10 @@
+# Creating a tuple
+coordinates = (4,5)
+# tuples are like lists(arrays), but they are immutable
+print(coordinates[1])
+
+# Tuples are data structures for storing data that should be immutable. We can also create a list of tuples
+map_coordinates = [(23, 45), (2, 7), (9, 0), (67, 23)]
+
+# Accessing the data in a list of tuples
+print(map_coordinates[0][0])
