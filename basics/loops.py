a2657db49a2a72a1e68575933fcf147438c11716
Martins Ngene <martinsngene@gmail.com>
2022-05-08 23:54:07 +0100

    while loops

---
+++ basics/loops.py
@@ -0,0 +1,30 @@
+# Writing a while loop
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+print("This is the end of the loop.")
+
+# Writing for loops
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# Iterating a string
+for x in "fruits":
+    print(x)
+
+# Using the 'break' keyword
+for x in "fruits":
+    print(x)
+    if x == "i":
+        break
+
+# Using the 'continue' keyword
+letters = ["A", "B", "C", "D", "E"]
+for x in letters:
+    if x == "C":
+        continue
+    print(x)
